4520897626a9e587023ea60861df10e67057cf45
Mario Miramontes <mmisthree@gmail.com>
2019-06-12 18:38:50 -0700

    routes for fetching and adding comments

--- functions/index.js
+++ functions/index.js
@@ -5,18 +5,34 @@ const app = express();
 
 const FBAuth = require("./util/fbauth");
 
-const { getAllScreams, postOneScream } = require("./routes/screams");
-const { signup, login, uploadImage } = require("./routes/users");
+const { 
+    getAllScreams, 
+    postOneScream, 
+    getScream, 
+    commentOnScream 
+} = require("./routes/screams");
+
+const { 
+    signup, 
+    login, 
+    uploadImage, 
+    addUserDetails, 
+    getAuthenticatedUser 
+} = require("./routes/users");
 
 
 // Scream routes
 app.get("/screams", getAllScreams)
 app.post("/scream", FBAuth, postOneScream);
+app.get('/scream/:screamId', getScream);
+app.post("/scream/:screamId/comment", FBAuth, commentOnScream);
 
 // user routes
 app.post("/signup", signup);
 app.post("/login", login);
 app.post("/user/image", FBAuth, uploadImage);
+app.post("/user", FBAuth, addUserDetails);
+app.get("/user", FBAuth, getAuthenticatedUser)
 
 
 exports.api = functions.https.onRequest(app);
--- functions/routes/users.js
+++ functions/routes/users.js
@@ -5,8 +5,9 @@ const config = require("../util/config");
 const firebase = require("firebase");
 firebase.initializeApp(config);
 
-const { validateSignupData, validateLoginData } = require("../util/validators");
+const { validateSignupData, validateLoginData, reduceUserDetails } = require("../util/validators");
 
+// Signup
 exports.signup = (req, res) => {
     const newUser = {
         email: req.body.email,
@@ -58,6 +59,7 @@ exports.signup = (req, res) => {
     });
 };
 
+// Login route
 exports.login = (req, res) => {
     const user = {
         email: req.body.email,
@@ -85,6 +87,44 @@ exports.login = (req, res) => {
     })
 };
 
+// Add user details
+exports.addUserDetails = (req, res) => {
+    let userDetails = reduceUserDetails(req.body);
+
+    db.doc(`/users/${req.user.handle}`).update(userDetails)
+    .then(() => {
+        return res.json({ message: "Details added successfully"});
+    })
+    .catch(err => {
+        console.log(err);
+        return res.status(500).json({ error: err.code });
+    })
+};
+
+// Get user details 
+exports.getAuthenticatedUser = (req, res) => {
+    let userData = {};
+    db.doc(`/users/${req.user.handle}`).get()
+    .then(doc => {
+        if(doc.exists) {
+            userData.credentials = doc.data()
+            return db.collection("likes").where("userHandle", "==", req.user.handle).get()
+        }
+    })
+    .then(data => {
+        userData.likes = [];
+        data.forEach(doc => {
+            userData.likes.push(doc.data());
+        })
+        return res.json(userData)
+    })
+    .catch(err => {
+        console.log(err)
+        return res.status(500).json({ error: err.code });
+    })
+};
+
+// Upload user profile image
 exports.uploadImage = (req, res) => {
     const BusBoy = require("busboy");
     const path = require("path");
